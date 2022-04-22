45d645247e000bb0dc40f42cfa04ed23863c8a9c
maxhugouhr <uhr.max@gmail.com>
2022-04-22 09:00:07 -0600

    Completed writeToFile method which intakes the primary blocking queue and write the body positions to a file with positions separated by commas and newlines separating each new timestamp.

--- src/Body.java
+++ src/Body.java
@@ -10,9 +10,8 @@ public class Body {
     private double[] velocity; //in units of length per second
     private double[] acceleration; //in units of length per second per second
     private double mass;
-    private static LinkedList<Body> bodies;
 
-    Body(double[] position, double[] velocity, double mass){
+    Body(Body[] bodies, double[] position, double[] velocity, double mass){
 
         super();
         this.identifier = objectCount;
@@ -21,7 +20,7 @@ public class Body {
         this.velocity = new double[]{velocity[0], velocity[1]};
         this.acceleration = new double[]{0, 0};
         this.mass = mass;
-        bodies.add(this);
+
 
     }
 
@@ -41,6 +40,8 @@ public class Body {
         return this.acceleration;
     }
 
+    public int getIdentifier(){return this.identifier;}
+
     public void updatePosition(double timeStep){
         //timeStep is the amount of time that is passing between each round of calculations in seconds
         this.velocity[0] += this.acceleration[0]*timeStep;
@@ -49,26 +50,29 @@ public class Body {
         this.position[1] += this.velocity[1]*timeStep;
     }
 
-    public void calculateAcceleration(){
+    public void calculateAcceleration(Body[] bodies){
         double[] forceVector = new double[]{0,0};
-        for (int i = 0; i < bodies.size(); i++){
+        for (int i = 0; i < bodies.length; i++){
             if (i == this.identifier){
                 continue;
             }
 
             //Calculates the unit vector pointing from this body to the body in the bodies array
+            //F12 = G*m1*m2*r12 / magR^2
             double[] radiusUnit = new double[2];
-            radiusUnit[0] = bodies.get(i).getPosition()[0] - this.position[0];
-            radiusUnit[1] = bodies.get(i).getPosition()[1] - this.position[1];
+            radiusUnit[0] = bodies[i].getPosition()[0] - this.position[0]; //x-component of radius vector
+            radiusUnit[1] = bodies[i].getPosition()[1] - this.position[1]; //y-component of radius vectur
             double radiusMag = Math.sqrt(Math.pow(radiusUnit[0],2) + Math.pow(radiusUnit[1],2));
+            //unit vector pointing from this body to the body applying the force
             radiusUnit[0] /= radiusMag;
             radiusUnit[1] /= radiusMag;
 
-            //Calculate the force being applied from the body in the array to this body
-            forceVector[0] += GRAVITY*this.mass*bodies.get(i).getMass()*radiusUnit[0]/radiusMag;
-            forceVector[1] += GRAVITY*this.mass*bodies.get(i).getMass()*radiusUnit[1]/radiusMag;
+            //sum the forces being applied from the body in the array to this body m1*m2*g*[r1,r2]/r^2
+            forceVector[0] += GRAVITY*this.mass*bodies[i].getMass()*radiusUnit[0]/Math.pow(radiusMag,2);
+            forceVector[1] += GRAVITY*this.mass*bodies[i].getMass()*radiusUnit[1]/Math.pow(radiusMag,2);
 
         }
+        //new acceleration based on the force being applied, acceleration = force / mass
         this.acceleration[0] = forceVector[0] / this.mass;
         this.acceleration[1] = forceVector[1] / this.mass;
     }
--- src/FileRunner.java
+++ src/FileRunner.java
@@ -1,31 +1,59 @@
+import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.util.concurrent.TimeUnit;
 
 //FileRunner class takes the positions of the bodies as they updates and stores
 // them in a file so they can be drawn by the Video class at a later time
 public class FileRunner {
 
-    private BlockingQueue<double[]> positionQueue;
-    private File myFile;
+    private String fileName;
 
-    FileRunner(BlockingQueue<double[]> q){
+    FileRunner(){
         super();
-        this.positionQueue = q;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a file name for storage:");
-        String filename = input.nextLine();
+        this.fileName = input.nextLine();
         try {
-            this.myFile = new File(filename);
-            if (myFile.createNewFile()) {
+            File nf = new File(this.fileName);
+            if (nf.createNewFile()) {
                 System.out.println("File created");
             } else {
                 System.out.println("File already exists");
             }
         } catch (IOException e) {
             System.out.println("Error occurred");
+            e.printStackTrace();
+        }
+    }
+
+    void writeToFile(BlockingQueue<ArrayList<Double>> positionQueue){
+        ArrayList<Double> line = null;
+        try{
+            line =  positionQueue.poll(1,TimeUnit.SECONDS);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        try{
+            FileWriter writer = new FileWriter(this.fileName);
+            BufferedWriter bw = new BufferedWriter(writer);
+            while (line != null) {
+                for (Double aDouble : line) {
+                    bw.write(String.valueOf(aDouble + ','));
+                }
+                bw.newLine();
+                try {
+                    line = positionQueue.poll(1,TimeUnit.SECONDS);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
