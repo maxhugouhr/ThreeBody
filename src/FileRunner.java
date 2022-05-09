1fde847805ee90913c315af8016fe7e64834f88f
maxhugouhr <uhr.max@gmail.com>
2022-05-09 09:32:06 -0600

    updated FileRunner to write objects in byte code with objectoutputstreams rather than a unique format

--- src/FileRunner.java
+++ src/FileRunner.java
@@ -1,12 +1,10 @@
+import javax.lang.model.type.ArrayType;
 import java.awt.*;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
-import java.io.FileWriter;
 import java.util.concurrent.TimeUnit;
 
 //FileRunner class takes the positions of the bodies as they update and stores
@@ -18,22 +16,17 @@ public class FileRunner {
     FileRunner(){
         super();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a file name for storage (ending in .txt):");
+        System.out.println("Enter a file name for storage (don't include file extension):");
         this.fileName = input.nextLine();
         try {
             File nf = new File(this.fileName);
-            int length = this.fileName.length();
-            if (nf.createNewFile() && this.fileName.substring(length-3, length).equals(".txt")) {
-                System.out.println("File created");
-            } else {
-                while (!nf.createNewFile() || !this.fileName.substring(length-3, length).equals(".txt")) {
+            while (!nf.createNewFile() || this.fileName.contains(".")) {
                     System.out.println("File already exists or format not followed, enter new name");
                     this.fileName = input.nextLine();
                     nf = new File(fileName);
-                }
-                System.out.println("File Created");
-
             }
+            this.fileName += ".txt";
+            System.out.println("File Created");
         } catch (IOException | StringIndexOutOfBoundsException e) {
             System.out.println("Error occurred");
             e.printStackTrace();
@@ -41,31 +34,28 @@ public class FileRunner {
     }
 
     // writeToFile intakes the queue that is being built by the simulation and adds the positions to the output storage file
-    void writeToFile(BlockingQueue<ArrayList<Double[]>> positionQueue){
-        ArrayList<Double[]> line = null; //list of body positions at a given timestamp
+    void writeToFile(BlockingQueue<ArrayList<Double>> positionQueue){
+        ArrayList<Double> line = null; //list of body positions at a given timestamp
         try{
-            line =  positionQueue.poll(1,TimeUnit.SECONDS); //runs until the simulation is complete
+            line =  positionQueue.poll(5,TimeUnit.SECONDS); //runs until the simulation is complete
         }catch (InterruptedException e){
             e.printStackTrace();
         }
 
         try{
-            FileWriter writer = new FileWriter(this.fileName, true);
-            BufferedWriter bw = new BufferedWriter(writer);
+            FileOutputStream writer = new FileOutputStream(this.fileName, true);
+            ObjectOutputStream os = new ObjectOutputStream(writer);
             while (line != null) {
-                bw.write('|');
-                for (Double[] position : line) {
-                    //writes the body positions (comma separated) separated by |
-                    bw.write(String.valueOf(position[0]) + ',' + String.valueOf(position[1]) + '|');
-                }
-                bw.newLine();//time steps are separated by line
+                os.writeObject(line);
                 try {
-                    line = positionQueue.poll(1,TimeUnit.SECONDS);
+                    line = positionQueue.poll(5,TimeUnit.SECONDS);
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            bw.close();
+            os.flush();
+            os.close();
+            writer.flush();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -73,31 +63,32 @@ public class FileRunner {
     }
 
     //writes out the masses of each body(so they can be sized) as well as the time information necessary to create a movie
-    void writeHeader(ArrayList<Body> bodies, int timeStep, TimeUnit unit) {
-        FileWriter fw = null;
-        BufferedWriter bw = null;
+    void writeHeader(ArrayList<Body> bodies, int timeStep) {
 
+        //writes the header for the file that will be turned into a gif
+        //format has the number of bodies and the time step as the first entry and the mass of each body as the second entry
         try {
-            fw = new FileWriter(this.fileName,false);
-            bw = new BufferedWriter(fw);
-            bw.write(timeStep + ',' + unit.toString()); //write the timestep and the units
-            bw.newLine();
+            FileOutputStream fw = new FileOutputStream(this.fileName);
+            ObjectOutputStream os = new ObjectOutputStream(fw);
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(bodies.size());
+            list.add(timeStep);
+            os.writeObject(list); //write the number of bodies being simulated and the timestep of the simulation
+            ArrayList<Double> bodyMass = new ArrayList<>();
             for (Body body : bodies) {
                 //writes the mass of each body, this will be used to size the spheres when
                 //drawing them into a gif
-                bw.write((int) body.getMass()+',');
+                bodyMass.add(body.getMass());
             }
+            os.writeObject(bodyMass);
+            os.flush();
+            os.close();
+            fw.flush();
+            fw.close();
 
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                bw.close();
-                fw.close();
-            } catch (NullPointerException | IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
